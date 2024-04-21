e01469ec60962e6f5bfed458f3b94b71a80f059b
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-21 09:59:12 +0530

    Update FH Read Txt file and display contents in console.py

--- FH Read Txt file and display contents in console.py
+++ FH Read Txt file and display contents in console.py
@@ -1,8 +1,11 @@
 #Task: Write a python function to read from a text file. The function will take the name of a text file and display its contents into the console.
-def readfile(filename): #Define function to read from text file
+def readfile(filename): 
+    #Define function to read from text file
     f= open(filename,'r')
     print(f.read())
     f.close()
 
-filename = input("Enter file name: ") #Enter the filename to be opened with .txt extension
-readfile(filename) #Call function with the input file name
+filename = input("Enter file name: ") 
+#Enter the filename to be opened with .txt extension
+readfile(filename) 
+#Call function with the input file name
