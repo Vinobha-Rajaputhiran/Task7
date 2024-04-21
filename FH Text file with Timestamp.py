3f6714089cc3d653667895a4229e8173ce387d7e
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-21 10:10:22 +0530

    Update FH Text file with Timestamp.py

--- FH Text file with Timestamp.py
+++ FH Text file with Timestamp.py
@@ -1,12 +1,16 @@
 #Task: Write a python program using function to create a text file with the current timestamp
 
-f=open('task7.txt','x') #Create a text file using python
+f=open('task7.txt','x') 
+#Create a text file using python
 f.close()
 
-def currenttimestamp(): #function to get the current system time stamp
-    import datetime #Get time stamp using datetime module. Time module can also be used.
+def currenttimestamp(): 
+    #function to get the current system time stamp
+    import datetime 
+    #Get time stamp using datetime module. Time module can also be used.
     ts= datetime.datetime.now()
-    return ts.strftime("%I:%M:%S %p")  # Return the timestamp value
+    return ts.strftime("%I:%M:%S %p")  
+    # Return the timestamp value
 
 f= open('task7.txt', 'w')
 f.write(currenttimestamp())
